34d9e1fb415917567d5f4fbba70bda6674f01a8a
SzBorkowski <s26076@pjwstk.edu.pl>
2023-08-02 21:39:06 +0200

    Progress

--- PracticePython/9.GuessingGameOne.py
+++ PracticePython/9.GuessingGameOne.py
@@ -1,14 +1,21 @@
-#work in progress
 import random
-while True:
+guess = 0
+while guess != "exit":
     x = random.randint(1, 9)
     print(x)
-    guess = "play"
-    while True and guess != "exit":
+    tries = 0
+    while guess != x and guess != "exit":
         guess = input("Guess a number between 1 and 9: ")
-        if int(guess) == x:
-            print("Correct!")
+        tries += 1
+        if guess == "exit":
             break
+        elif int(guess) == x:
+            if tries == 1:
+                print("Correct! You guessed the number in ",tries,"try!")
+                break
+            else:
+                print("Correct! You guessed the number in ",tries,"tries!")
+                break
         elif int(guess) > x:
             print("Too high")
         elif int(guess) < x:
