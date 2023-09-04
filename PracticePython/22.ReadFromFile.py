75b963c3d1d9e8660d8d5fdc2ae8b6203459de7e
SzBorkowski <s26076@pjwstk.edu.pl>
2023-09-04 22:29:32 +0200

    Progress

--- PracticePython/22.ReadFromFile.py
+++ PracticePython/22.ReadFromFile.py
@@ -1,10 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://www.practicepython.org/assets/nameslist.txt'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, features="html.parser")
-
-with open(r, 'r') as f:
-    all_text = f.read()
-print(all_text)
+with open('filename.txt') as f:
+  	line = f.readline()
+  	while line:
+  		print(line)
+  		line = f.readline()
