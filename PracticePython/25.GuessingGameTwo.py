ffb40220526cb6c6758980fde88553e8714d1983
SzBorkowski <s26076@pjwstk.edu.pl>
2023-09-10 23:25:51 +0200

    Progress

--- PracticePython/25.GuessingGameTwo.py
+++ PracticePython/25.GuessingGameTwo.py
@@ -1,16 +1,4 @@
 import random
-"""
-input("Pick a number between 0 and 100 and I will try to guess it.")
-guess = random.randint(0, 100)
-while True:
-    decision = input("Is it "+str(guess)+"? (Correct, too high, too low) ")
-    if decision == "correct":
-        print("Yay!")
-        break
-    if decision == "too high":
-        guess = random.randint(0, guess)
-    if decision == "too low":
-        guess = random.randint(guess, 100)"""
 
 input("Pick a number between 0 and 100 and I will try to guess it.")
 guess = random.randint(0, 100)
@@ -28,9 +16,9 @@ while True:
                 break
         guess = random.choice(guesslist)
     if decision == "too low":
-        while True:
-            guesslist.remove(guess)
-            guess -= 1
-            if guess == guesslist[0]:
-                break
+        i = 0
+        while i < guess:
+            del guesslist[i]
+            i += 1
         guess = random.choice(guesslist)
+#Why do the loops work only on odd indexes???
