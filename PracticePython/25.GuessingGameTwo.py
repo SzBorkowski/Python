7c0357dadd718cec72d16d09ca78f01672a7052b
SzBorkowski <s26076@pjwstk.edu.pl>
2023-09-11 23:07:32 +0200

    Progress

--- PracticePython/25.GuessingGameTwo.py
+++ PracticePython/25.GuessingGameTwo.py
@@ -3,26 +3,31 @@ import random
 input("Pick a number between 0 and 100 and I will try to guess it.")
 guess = random.randint(0, 100)
 guesslist = list(range(0, 101))
-print(guesslist)
+tries = 0
 while True:
     decision = input("Is it "+str(guess)+"? (Correct, too high, too low) ")
     if decision == "correct":
-        print("Yay!")
+        tries += 1
+        if tries == 1:
+            print("Wow! It took me " + str(tries) + " try to guess it!")
+        print("Yay! It took me "+str(tries)+" tries to guess it!")
         break
     if decision == "too high":
+        tries += 1
         if guess == 0:
             print("But that's the lowest number possible!")
             break
         i = guess
         length = len(guesslist)
         while True:
+            if guess == guesslist[-1]:
+                guesslist.remove(guess)
+                break
             guesslist.remove(guess)
             guess += 1
-            if guess == length:
-                break
         guess = random.randint(guesslist[0], i-1)
-        print(guesslist)
     if decision == "too low":
+        tries += 1
         if guess == 100:
             print("But that's the highest number possible!")
             break
@@ -34,5 +39,3 @@ while True:
             guesslist.remove(guess)
             guess -= 1
         guess = random.randint(i+1, guesslist[-1])
-        print(guesslist)
-        #Almost there
