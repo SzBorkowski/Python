ec08eaec0c04401fca8f04684c30a59da1f98cfd
SzBorkowski <s26076@pjwstk.edu.pl>
2023-12-20 21:57:32 +0100

    Finished day 15

--- 100 Days of Code/Day 15 - Coffee Machine.py
+++ 100 Days of Code/Day 15 - Coffee Machine.py
@@ -33,15 +33,21 @@ resources = {
 
 
 def res_check(coffe_type):
+    global resources, machine
     if MENU[coffe_type]["ingredients"]["water"] > resources["water"]:
         print("Not enough water.")
         machine = "no resources"
-    if MENU[coffe_type]["ingredients"]["milk"] > resources["milk"]:
+    elif MENU[coffe_type]["ingredients"]["milk"] > resources["milk"]:
         print("Not enough milk.")
         machine = "no resources"
-    if MENU[coffe_type]["ingredients"]["coffee"] > resources["coffee"]:
+    elif MENU[coffe_type]["ingredients"]["coffee"] > resources["coffee"]:
         print("Not enough coffee.")
         machine = "no resources"
+    else:
+        for key in resources:
+            if key == "money":
+                break
+            resources[key] -= MENU[coffe_type]["ingredients"][key]
 
 
 def money(coffe_type):
@@ -50,12 +56,14 @@ def money(coffe_type):
     dimes = float(input("Insert dimes: "))
     nickels = float(input("Insert nickels: "))
     pennies = float(input("Insert pennies: "))
-    sum = quarters*0.25 + dimes*0.10 + nickels*0.05 + pennies*0.01
+    sum = quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
     if sum < MENU[coffe_type]["cost"]:
         print("Sorry that's not enough money. Money refunded.")
     else:
         change = sum - MENU[coffe_type]["cost"]
         resources["money"] += MENU[coffe_type]["cost"]
+        print(f"Here is ${round(change, 2)} dollars in change.")
+
 
 # Main loop
 machine = "working"
@@ -67,4 +75,7 @@ while machine == "working":
         print(resources)
     else:
         res_check(decision)
-        money(decision)
+        if machine == "working":
+            money(decision)
+            print(f"Here is your {decision}, enjoy!")
+        machine = "working"
