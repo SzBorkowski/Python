28dab7ddefa53c1b44148d0ec270beb3fe12f1ba
Szymon Borkowski <s26076@pjwstk.edu.pl>
2023-12-21 23:13:27 +0100

    Fixed the typos

--- 100 Days of Code/Day 15 - Coffee Machine.py
+++ 100 Days of Code/Day 15 - Coffee Machine.py
@@ -32,36 +32,36 @@ resources = {
 }
 
 
-def res_check(coffe_type):
+def res_check(coffee_type):
     global resources, machine
-    if MENU[coffe_type]["ingredients"]["water"] > resources["water"]:
+    if MENU[coffee_type]["ingredients"]["water"] > resources["water"]:
         print("Not enough water.")
         machine = "no resources"
-    elif MENU[coffe_type]["ingredients"]["milk"] > resources["milk"]:
+    elif MENU[coffee_type]["ingredients"]["milk"] > resources["milk"]:
         print("Not enough milk.")
         machine = "no resources"
-    elif MENU[coffe_type]["ingredients"]["coffee"] > resources["coffee"]:
+    elif MENU[coffee_type]["ingredients"]["coffee"] > resources["coffee"]:
         print("Not enough coffee.")
         machine = "no resources"
     else:
         for key in resources:
             if key == "money":
                 break
-            resources[key] -= MENU[coffe_type]["ingredients"][key]
+            resources[key] -= MENU[coffee_type]["ingredients"][key]
 
 
-def money(coffe_type):
+def money(coffee_type):
     global resources
     quarters = float(input("Insert quarters: "))
     dimes = float(input("Insert dimes: "))
     nickels = float(input("Insert nickels: "))
     pennies = float(input("Insert pennies: "))
-    sum = quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
-    if sum < MENU[coffe_type]["cost"]:
+    total = quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
+    if total < MENU[coffee_type]["cost"]:
         print("Sorry that's not enough money. Money refunded.")
     else:
-        change = sum - MENU[coffe_type]["cost"]
-        resources["money"] += MENU[coffe_type]["cost"]
+        change = total - MENU[coffee_type]["cost"]
+        resources["money"] += MENU[coffee_type]["cost"]
         print(f"Here is ${round(change, 2)} dollars in change.")
 
 
