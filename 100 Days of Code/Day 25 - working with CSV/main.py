f7b97b160fdff4984ea7a3f2054e86e359ad31c2
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-03-04 20:02:29 +0100

    Progress

--- 100 Days of Code/Day 25 - working with CSV/main.py
+++ 100 Days of Code/Day 25 - working with CSV/main.py
@@ -1,4 +1,4 @@
-import csv
+import pandas
 import turtle
 
 screen = turtle.Screen()
@@ -7,25 +7,24 @@ image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
 
-with open("50_states.csv") as data_file:
-    states_data = csv.reader(data_file)
-    states = []
-    for row in states_data:
-         if row[0] != "state":
-             states.append(row[0])
-
-states_lowercase = [i.lower() for i in states]
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
 score = 0
 title = "Guess the State"
 
-
-while True:
-    answer_state = screen.textinput(title=title, prompt="What's another state's name?")
-    if answer_state.lower() in states_lowercase:
-        states_lowercase.remove(answer_state.lower())
+while len(all_states) > 0:
+    answer_state = screen.textinput(title=title, prompt="What's another state's name?").title()
+    if answer_state in all_states:
         score += 1
         title = f"{score}/50 States Correct"
-
-# Niepotrzebnie zmieniałem stany do listy
-# Potrzebne są współrzędne
-# Oraz stany z wielkiej litery żeby je wypisać na mapie
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.pu()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+        all_states.remove(answer_state)
+    if answer_state == "Exit":
+        new_data = pandas.DataFrame(all_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
