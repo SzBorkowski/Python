8f72afadde8d72b819196ca3fc89d03cea24eb08
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-03-04 19:10:21 +0100

    Progress

--- 100 Days of Code/Day 25 - working with CSV/main.py
+++ 100 Days of Code/Day 25 - working with CSV/main.py
@@ -1,3 +1,4 @@
+import csv
 import turtle
 
 screen = turtle.Screen()
@@ -6,6 +7,25 @@ image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+with open("50_states.csv") as data_file:
+    states_data = csv.reader(data_file)
+    states = []
+    for row in states_data:
+         if row[0] != "state":
+             states.append(row[0])
 
-turtle.mainloop()
+states_lowercase = [i.lower() for i in states]
+score = 0
+title = "Guess the State"
+
+
+while True:
+    answer_state = screen.textinput(title=title, prompt="What's another state's name?")
+    if answer_state.lower() in states_lowercase:
+        states_lowercase.remove(answer_state.lower())
+        score += 1
+        title = f"{score}/50 States Correct"
+
+# Niepotrzebnie zmieniałem stany do listy
+# Potrzebne są współrzędne
+# Oraz stany z wielkiej litery żeby je wypisać na mapie
