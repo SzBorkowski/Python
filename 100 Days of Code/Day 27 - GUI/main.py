38d7dbc6f304bd045a862f669d608a5b0dffe06c
SzBorkowski <s26076@pjwstk.edu.pl>
2024-03-10 20:58:55 +0100

    Progress

--- 100 Days of Code/Day 27 - GUI/main.py
+++ 100 Days of Code/Day 27 - GUI/main.py
@@ -1,19 +1,40 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
+my_label["text"] = "New Text"
+# OR
+my_label.config(text="New Text")
+
+
+# Button
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
+input.get()
 
 window.mainloop()
+
 
 def add(*args):
     sum = 0
     for n in args:
         sum += n
     print(sum)
+
 
 add(1, 2, 3, 4)
