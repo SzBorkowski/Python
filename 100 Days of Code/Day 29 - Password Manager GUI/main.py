68ce01a478ccded6a396705c9c12020f86255af8
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-03-15 22:01:36 +0100

    Progress

--- 100 Days of Code/Day 29 - Password Manager GUI/main.py
+++ 100 Days of Code/Day 29 - Password Manager GUI/main.py
@@ -1,8 +1,49 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    pass_entry.delete(0, END)
+    pass_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = pass_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\n"
+        f"Email: {email} \nPassword: {password} \nIs it ok to save?")
+
+        if is_ok:
+            with open("data.txt", "a") as f:
+                f.write(f"{website} | {email} | {password}\n")
+
+            website_entry.delete(0, END)
+            pass_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -25,15 +66,17 @@ pass_label.grid(column=0, row=3)
 # Entries
 website_entry = Entry(width=52)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=52)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "szymon.borkowski7@wp.pl")
 pass_entry = Entry(width=34)
 pass_entry.grid(column=1, row=3)
 
 # Buttons
-pass_gen_button = Button(text="Generate Password")
+pass_gen_button = Button(text="Generate Password", command=generate_password)
 pass_gen_button.grid(column=2, row=3)
-add_button = Button(text="Add", width=44)
+add_button = Button(text="Add", width=44, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
