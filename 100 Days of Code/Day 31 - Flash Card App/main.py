2abcdb8bef74029cf02cdf83caa0a5870a4b367b
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-03-18 21:10:39 +0100

    Progress

--- 100 Days of Code/Day 31 - Flash Card App/main.py
+++ 100 Days of Code/Day 31 - Flash Card App/main.py
@@ -1,9 +1,18 @@
 from tkinter import *
 import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-# UI
+data = pandas.read_csv("wordbank.csv")
+to_learn = data.to_dict(orient="records")
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="Italian")
+    canvas.itemconfig(card_word, text = current_card["Italian"])
+
+
 window = Tk()
 window.title("Flash Cards")
 window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -11,20 +20,18 @@ window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)
 front_img = PhotoImage(file="images/card_front.png")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=front_img)
-canvas.create_text(400, 150, text="Language", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 right_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_img, highlightthickness=0)
+right_button = Button(image=right_img, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 
-# Reading values
-data = pandas.read_csv("wordbank.csv")
-
+next_card()
 
 window.mainloop()
