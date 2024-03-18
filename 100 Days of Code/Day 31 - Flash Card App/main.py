e7a925a11e9c8a453325d93c5125ec6ef36725e7
SzBorkowski <s26076@pjwstk.edu.pl>
2024-03-18 22:59:59 +0100

    Progress

--- 100 Days of Code/Day 31 - Flash Card App/main.py
+++ 100 Days of Code/Day 31 - Flash Card App/main.py
@@ -3,23 +3,51 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
-data = pandas.read_csv("wordbank.csv")
-to_learn = data.to_dict(orient="records")
+try:
+    data = pandas.read_csv("wordbank.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("wordbank.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="Italian")
-    canvas.itemconfig(card_word, text = current_card["Italian"])
+    canvas.itemconfig(canvas_image, image=front_img)
+    canvas.itemconfig(card_title, text="Italian", fill="black")
+    canvas.itemconfig(card_word, text=current_card["Italian"], fill="black")
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    global current_card
+    canvas.itemconfig(canvas_image, image=back_img)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
+def remove_card():
+    to_learn.remove(current_card)
+    data = pandas. DataFrame(to_learn)
+    data.to_csv("words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
 window.title("Flash Cards")
 window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 front_img = PhotoImage(file="images/card_front.png")
+back_img = PhotoImage(file="images/card_back.png")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_image(400, 263, image=front_img)
+canvas_image = canvas.create_image(400, 263, image=front_img)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
@@ -29,7 +57,7 @@ wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 right_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_img, highlightthickness=0, command=next_card)
+right_button = Button(image=right_img, highlightthickness=0, command=remove_card)
 right_button.grid(column=1, row=1)
 
 next_card()
