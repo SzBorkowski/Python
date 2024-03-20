614551ba05cebad09d4df58a5de782bac528ddf6
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-03-20 23:29:33 +0100

    Progress

--- 100 Days of Code/Day 32 - Sending emails/main.py
+++ 100 Days of Code/Day 32 - Sending emails/main.py
@@ -1,12 +1,20 @@
-import smtplib
+# import smtplib
+#
+# my_email = "pythn688@gmail.com"
+# password = "pesw wsfa ydmm pols"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs="simon345@wp.pl",
+#                         msg="Subject:Hello!\n\nEmail body")
+#
+import datetime as dt
 
-my_email = "pythn688@gmail.com"
-password = "pesw wsfa ydmm pols"
+now = dt.datetime.now()
+year = now.year
+print(year)
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="simon345@wp.pl",
-                        msg="Subject:Hello!\n\nEmail body")
-
+date_of_birth = dt.datetime(year=1997, month=7, day=6)
+print(date_of_birth)
