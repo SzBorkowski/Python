577ee4392a1b0416dff7e184bd8e0f343d914236
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-03-25 19:06:57 +0100

    Progress

--- 100 Days of Code/Day 34 - GUI Quiz App/ui.py
+++ 100 Days of Code/Day 34 - GUI Quiz App/ui.py
@@ -1,2 +1,17 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+        self.canvas = Canvas()
+        self.canvas.config(width=300, height=250)
+        self.canvas.grid()
+        
+
+
+        self.window.mainloop()
