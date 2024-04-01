d894be58e6caf4260b754c79895bcf2933d480c3
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-04-01 21:10:31 +0200

    Progress

--- 100 Days of Code/Day 35 - SMS weather alert/main.py
+++ 100 Days of Code/Day 35 - SMS weather alert/main.py
@@ -1,9 +1,13 @@
 import requests
+from twilio.rest import Client
 
 # https://api.openweathermap.org/data/2.5/weather?lat=52.229675&lon=21.012230&appid=f5b7565e36039b5af8bf5edb254b87e1
 
 OMW_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "f5b7565e36039b5af8bf5edb254b87e1"
+api_key = input("Input api_key: ")
+account_sid = input("Input account_sid: ")
+auth_token = input("Input your auth_token: ")
+verified_phone = input("Input your verified phone number: ")
 params = {
     "lat": 52.229675,
     "lon": 21.012230,
@@ -22,4 +26,11 @@ for forecast in range(params["cnt"]):
     if weather_data["list"][forecast]["weather"][0]["id"] < 700:
         will_rain = True
 if will_rain:
-    print("Bring an umbrella.")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an umbrella.",
+        from_="+12055493689",
+        to=verified_phone
+    )
+    print(message.status)
