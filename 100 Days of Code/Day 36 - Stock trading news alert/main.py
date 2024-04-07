421e21340f2b7a7c336c20ba16e16961f0c49e69
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-04-07 16:10:20 +0200

    Progress

--- 100 Days of Code/Day 36 - Stock trading news alert/main.py
+++ 100 Days of Code/Day 36 - Stock trading news alert/main.py
@@ -1,12 +1,11 @@
 import os
 import requests
-from datetime import date
-from datetime import timedelta
+from datetime import date, timedelta, datetime
 from twilio.rest import Client
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-
+TREND = "🔺"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 STOCK_KEY = "4ZE2OHP2NTWBMLRX"
 STOCK_PARAMS = {
@@ -24,15 +23,21 @@ account_sid = "ACb65f258128faaea33e133366265f11a4"
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 verified_phone = os.environ['TWILIO_VERIFIED_PHONE']
 
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-# HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-# HINT 2: Work out the value of 5% of yerstday's closing stock price.
+
+# Getting info about stock price movement
 response = requests.get(STOCK_ENDPOINT, params=STOCK_PARAMS)
 response.raise_for_status()
 data = response.json()
 
+# Setting the dates for today, yesterday
 today = date.today()
+today_weekday = today.weekday()
+while True:
+    if today_weekday > 5:
+        today -= timedelta(days=1)
+        today_weekday = today.weekday()
+    else:
+        break
 yesterday = today - timedelta(days=1)
 b4yesterday = today - timedelta(days=2)
 
@@ -48,49 +53,27 @@ def get_news():
 
 
 def send_sms():
-    global article1, article2, article3
+    global article1, article2, article3, percentage_diff
     client = Client(account_sid, auth_token)
     message = client.messages \
         .create(
-        body=f"{article1}\n{article2}\n{article3}",
+        body=f"{STOCK}: {TREND}{int(percentage_diff)}%\n{article1}\n{article2}\n{article3}",
         from_="+12055493689",
         to=verified_phone
     )
     print(message.status)
 
 
+# Main code, checking price movements and sending SMS if it's more than 5%
 ystday_price = data["Time Series (Daily)"][f"{yesterday}"]["4. close"]
 b4ystday_price = data["Time Series (Daily)"][f"{b4yesterday}"]["4. close"]
 price_diff = float(ystday_price) - float(b4ystday_price)
 if price_diff < 0:
+    TREND = "🔻"
     price_diff *= -1
+percentage_diff = price_diff / float(ystday_price) * 100
 trigger = float(ystday_price) * 0.05
 if price_diff > trigger:
     print("Get News")
     get_news()
     send_sms()
-get_news()
-send_sms()
-
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-# HINT 1: Think about using the Python Slice Operator
-
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-# HINT 1: Consider using a List Comprehension.
-account_sid = "ACb65f258128faaea33e133366265f11a4"
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-verified_phone = os.environ['TWILIO_VERIFIED_PHONE']
-
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
