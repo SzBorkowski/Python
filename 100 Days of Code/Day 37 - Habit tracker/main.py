5046d1240b4ea19e136c461f9dc9d66e872d2b17
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-04-09 22:40:50 +0200

    Progress

---
+++ 100 Days of Code/Day 37 - Habit tracker/main.py
@@ -0,0 +1,35 @@
+import requests
+
+
+USERNAME = "franku"
+TOKEN = "qwertyuiop1234567890"
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# Setting up a new account
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+# Setting up a new graph
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
