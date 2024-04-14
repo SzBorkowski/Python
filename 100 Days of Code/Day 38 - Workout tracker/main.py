2a6b681d1380c6e56d79b3682cedb022162704fc
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-04-14 19:17:29 +0200

    Progress

--- 100 Days of Code/Day 38 - Workout tracker/main.py
+++ 100 Days of Code/Day 38 - Workout tracker/main.py
@@ -1,2 +1,29 @@
+import requests
+
 APP_ID = "0779905c"
 API_KEY = "7562f0773d582ba7884e40504dd9adb3"
+GENDER = "male"
+WEIGHT_KG = 69
+HEIGHT_CM = 175
+AGE = 24
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
