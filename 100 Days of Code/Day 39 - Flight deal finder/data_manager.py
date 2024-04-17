117539ad871063214a2659f1ed123dac28d28139
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-04-17 22:09:41 +0200

    Progress

--- 100 Days of Code/Day 39 - Flight deal finder/data_manager.py
+++ 100 Days of Code/Day 39 - Flight deal finder/data_manager.py
@@ -1,3 +1,4 @@
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
+    sheet_endpoint = "https://api.sheety.co/3fc742bbb27845fda453ab66b27fc19e/flightDeals/prices"
     pass
