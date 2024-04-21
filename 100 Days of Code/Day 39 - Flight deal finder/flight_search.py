53bca0e1f56475cf396a059a89ba39eab334e52f
Szymon Borkowski <s26076@pjwstk.edu.pl>
2024-04-21 12:26:03 +0200

    Progress

--- 100 Days of Code/Day 39 - Flight deal finder/flight_search.py
+++ 100 Days of Code/Day 39 - Flight deal finder/flight_search.py
@@ -6,6 +6,10 @@ TEQUILA_API_KEY = "I477GsTeCU0zv5A-wNFHiJf9tY2uKvjC"
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        code = "TESTING"
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
